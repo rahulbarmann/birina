73973fc61603790fb6038eaff62485c6a1028c0f
rahulbarmann <itsrahulbarman1@gmail.com>
2024-12-12 22:10:43 +0530

    refactor: setup util files and env vars

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    env: {
+        PINATA_API_KEY: process.env.PINATA_API_KEY,
+        PINATA_SECRET_KEY: process.env.PINATA_SECRET_KEY,
+    },
 };
 
 export default nextConfig;
